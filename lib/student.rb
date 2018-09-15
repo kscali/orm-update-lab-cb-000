289f503e687358464ed31e6580b151e03f47ab92
Koy Saeteurn <kosatun@aol.com>
2018-09-15 05:15:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,15 +54,21 @@ class Student
   
   def self.find_by_name(name)
     sql = <<-SQL
-        SELECT * FROM students WHERE name = ? LIMIT l
-      SQL
-      DB[:conn].execute(sql, name).map do|row|
-        self.new_from_db(row)
-      end.first  
-  end  
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
   
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE students 
+    SET name = ?, 
+    grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end   
 end
